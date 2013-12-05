b25d1ad6c5a9ad845e514a9ec25c2ae6f6ff51a4
JaHoFalk <janik.hopf@web.de>
2013-12-05 19:44:13 +0100

    Layout Change

--- public_html/app/views/Fach.js
+++ public_html/app/views/Fach.js
@@ -3,15 +3,25 @@ define(function(require) {
     "use strict";
 
     var Backbone = require('backbone');
-    var template = require('hbs!app/tpl/Missing');
-    
+    var template = require('hbs!app/tpl/Fach');
+
     return Backbone.View.extend({
         initialize: function() {
             this.render();
         },
+        events: {
+            "click #ok": "notensave",
+        },
+        notensave: function() {
+            var punkte = this.$el.find("#punkte")
+                    .val();
+            var note = this.$el.find("#note")
+                    .val();
+            console.log("Eingabe Punkte: " + punkte + " Eingabe Note: " + note);
+        },
         render: function() {
             // render table
-            this.$el.html(template({title:'Fach'}));
+            this.$el.html(template());
             return this;
         }
     });
